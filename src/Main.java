09757c8c36c1b0ff5e16898306bd0344e5823cd2
carlos <charlie23garrido@gmail.com>
2023-05-28 22:23:56 +0200

    Esto es un commit 2 pa

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("hola bro");
+            System.out.println("hola bro 2 ");
 
         }
     }
